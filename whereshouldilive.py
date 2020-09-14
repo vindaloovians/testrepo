e259a02a387e444808e2eaedd485e2521cb0bc0f
vindaloovians <70905266+vindaloovians@users.noreply.github.com>
2020-09-14 10:11:11 +0100

    Add files via upload

---
+++ whereshouldilive.py
@@ -0,0 +1,439 @@
+"""
+The factors that will be considered for each country are:
+    HDI # as wikipedia
+    Gay rights # wikipedia. 1 for recognised unions + trans rights, 2 for legal/descriminalised, 3 for illegal, 4 for death penalty 
+    GDP Per Capita (PPP) # according to world bank
+    Population Size
+    Continent
+    English language
+    
+    Women's rights
+"""
+Afghanistan = ["Afghanistan",4,4,5]
+Albania = ["Albania",2,2,3]
+Algeria = ["Algeria",2,3,3]
+Andorra = ["Andorra",1,1]
+Angola = ["Angola",3,2,4]
+AntiguaandBarbuda = ["AntiguaandBarbuda",2,3,2]
+Argentina = ["Argentina",1,1,2]
+Armenia = ["Armenia",2,2,3]
+Australia = ["Australia",1,0,1]
+Austria = ["Austria",1,0,1]
+Azerbaijan = ["Azerbaijan",2,2,2]
+Bahamas = ["Bahamas",1,2,2]
+Bahrain = ["Bahrain",1,2,1]
+Bangladesh = ["Bangladesh",3,3,4]
+Barbados = ["Barbados",1,3,2]
+Belarus = ["Belarus",1,2,2]
+Belgium = ["Belgium",1,0,1]
+Belize = ["Belize",2,2,3]
+Benin = ["Benin",4,2,5]
+Bhutan = ["Bhutan",3,3,3]
+Bolivia = ["Bolivia",2,2,3]
+BosniaandHerzegovina = ["BosniaandHerzegovina",2,2,3]
+Botswana = ["Botswana",2,2,2]
+Brazil = ["Brazil",2,0,3]
+Brunei = ["Brunei",1,4,1]
+Bulgaria = ["Bulgaria",1,1,2]
+BurkinaFaso = ["BurkinaFaso",4,2,5]
+Burundi = ["Burundi",4,3,5]
+CapeVerde = ["CapeVerde",3,2,]
+Cambodia = ["Cambodia",3,2]
+Cameroon = ["Cameroon",3,3]
+Canada = ["Canada",1,0]
+CAR = ["CAR",4,2]
+Chad = ["Chad",4,3]
+Chile = ["Chile",1,1]
+China = ["China",2,2]
+Colombia = ["Colombia",2,0]
+Comoros = ["Comoros",4,3]
+DRC = ["DRC",4,2]
+ROC = ["ROC",3,2]
+CostaRica = ["CostaRica",2,1]
+CotedIvoire = ["CotedIvoire",4,2]
+Croatia = ["Croatia",1,1]
+Cuba = ["Cuba",2,2]
+Cyprus = ["Cyprus",1,1]
+Czechia = ["Czechia",1,1]
+Denmark = ["Denmark",1,0]
+Djibouti = ["Djibouti",4,2]
+Dominica = ["Dominica",2,3]
+DominicanRepublic = ["DominicanRepublic",2,2]
+EastTimor = ["EastTimor",3,2]
+Ecuador = ["Ecuador",2,1]
+Egypt = ["Egypt",2,3]
+ElSalvador = ["ElSalvador",3,2]
+EquatorialGuinea = ["EquatorialGuinea",3,2]
+Eritrea = ["Eritrea",4,4]
+Estonia = ["Estonia",1,1]
+Eswatini = ["Eswatini",3,4]
+Ethiopia = ["Ethiopia",4,3]
+Fiji = ["Fiji",2,2]
+Finland = ["Finland",1,0]
+France = ["France",1,0]
+Gabon = ["Gabon",2,3]
+Gambia = ["Gambia",4,3]
+Georgia = ["Georgia",2,2]
+Germany = ["Germany",1,0]
+Ghana = ["Ghana",3,3]
+Greece = ["Greece",1,1]
+Grenada = ["Grenada",2,3]
+Guatemala = ["Guatemala",3,2]
+Guinea = ["Guinea",4,3]
+GuineaBissau = ["GuineaBissau",4,2]
+Guyana = ["Guyana",3,3]
+Haiti = ["Haiti",4,2]
+Honduras = ["Honduras",3,2]
+HongKong = ["HongKong",1,2]
+Hungary = ["Hungary",1,1]
+Iceland = ["Iceland",1,0]
+India = ["India",3,2]
+Indonesia = ["Indonesia",2,2]
+Iran = ["Iran",2,3]
+Iraq = ["Iraq",3,2]
+Ireland = ["Ireland",1,0]
+Israel = ["Israel",1,1]
+Italy = ["Italy",1,1]
+Jamaica = ["Jamaica",2,3]
+Japan = ["Japan",1,2]
+Jordan = ["Jordan",2,2]
+Kazakhstan = ["Kazakhstan",1,2]
+Kenya = ["Kenya",3,3]
+Kiribati = ["Kiribati",3,3]
+Kosovo = ["Kosovo",4,2]
+Kuwait = ["Kuwait",1,3]
+Kyrgyzstan = ["Kyrgyzstan",3,2]
+Laos = ["Laos",3,2]
+Latvia = ["Latvia",1,2]
+Lebanon = ["Lebanon",2,3]
+Lesotho = ["Lesotho",4,2]
+Liberia = ["Liberia",4,3]
+Libya = ["Libya",2,3]
+Liechtenstein = ["Liechtenstein",1,2]
+Lithuania = ["Lithuania",1,2]
+Luxembourg = ["Luxembourg",1,0]
+Madagascar = ["Madagascar",4,2]
+Malawi = ["Malawi",4,3]
+Malaysia = ["Malaysia",1,3]
+Maldives = ["Maldives",2,3]
+Mali = ["Mali",4,2]
+Malta = ["Malta",1,0]
+MarshallIslands = ["MarshallIslands",3,2]
+Mauritania = ["Mauritania",4,4]
+Mauritius = ["Mauritius",2,2]
+Mexico = ["Mexico",2,1]
+Micronesia = ["Micronesia",3,2]
+Moldova = ["Moldova",2,2]
+Monaco = ["Monaco",4,1]
+Mongolia = ["Mongolia",2,2]
+Montenegro = ["Montenegro",1,2]
+Morocco = ["Morocco",3,3]
+Mozambique = ["Mozambique",4,2]
+Myanmar = ["Myanmar",3,3]
+Namibia = ["Namibia",3,2]
+Nauru = ["Nauru",4,2]
+Nepal = ["Nepal",3,2]
+Netherlands = ["Netherlands",1,0]
+NewZealand = ["NewZealand",1,0]
+Nicaragua = ["Nicaragua",3,2]
+Niger = ["Niger",4,2]
+Nigeria = ["Nigeria",4,4]
+NorthKorea = ["NorthKorea",4,2]
+NorthMacedonia = ["NorthMacedonia",2,2]
+Norway = ["Norway",1,0]
+Oman = ["Oman",1,3]
+Pakistan = ["Pakistan",3,3]
+Palau = ["Palau",1,2]
+Palestine = ["Palestine",3,3]
+Panama = ["Panama",2,2]
+PapuaNewGuinea = ["PapuaNewGuinea",4,3]
+Paraguay = ["Paraguay",2,2]
+Peru = ["Peru",2,1]
+Philippines = ["Philippines",2,2]
+Poland = ["Poland",1,1]
+Portugal = ["Portugal",1,0]
+Qatar = ["Qatar",1,4]
+Romania = ["Romania",1,2]
+Russia = ["Russia",1,2]
+Rwanda = ["Rwanda",4,2]
+SaintKittsandNevis = ["SaintKittsandNevis",2,3]
+SaintLucia = ["SaintLucia",2,3]
+SaintVincentandtheGrenadines = ["SaintVincentandtheGrenadines",2,3]
+Samoa = ["Samoa",2,3]
+SanMarino = ["SanMarino",4,1]
+SaoTomeandPrincipe = ["SaoTomeandPrincipe",3,2]
+SaudiArabia = ["SaudiArabia",1,4]
+Senegal = ["Senegal",4,3]
+Serbia = ["Serbia",2,2]
+Seychelles = ["Seychelles",1,2]
+SierraLeone = ["SierraLeone",4,3]
+Singapore = ["Singapore",1,3]
+Slovakia = ["Slovakia",1,1]
+Slovenia = ["Slovenia",1,1]
+SolomonIslands = ["SolomonIslands",3,3]
+Somalia = ["Somalia",4,4]
+SouthAfrica = ["SouthAfrica",2,0]
+SouthKorea = ["SouthKorea",1,2]
+SouthSudan = ["SouthSudan",4,3]
+Spain = ["Spain",1,0]
+SriLanka = ["SriLanka",2,2]
+Sudan = ["Sudan",4,4]
+Suriname = ["Suriname",2,2]
+Sweden = ["Sweden",1,0]
+Switzerland = ["Switzerland",1,1]
+Syria = ["Syria",4,3]
+Taiwan = ["Taiwan",1,1]
+Tajikistan = ["Tajikistan",3,2]
+Tanzania = ["Tanzania",4,4]
+Thailand = ["Thailand",2,2]
+Togo = ["Togo",4,3]
+Tonga = ["Tonga",2,3]
+TrinidadandTobago = ["TrinidadandTobago",2,2]
+Tunisia = ["Tunisia",2,3]
+Turkey = ["Turkey",1,2]
+Turkmenistan = ["Turkmenistan",2,3]
+Tuvalu = ["Tuvalu",4,3]
+Uganda = ["Uganda",4,4]
+Ukraine = ["Ukraine",2,2]
+UAE = ["UAE",1,4]
+UK = ["UK",1,0]
+USA = ["USA",1,1]
+Uruguay = ["Uruguay",1,0]
+Uzbekistan = ["Uzbekistan",2,3]
+Vanuatu = ["Vanuatu",3,2]
+VaticanCity = ["VaticanCity",4,2]
+Venezuela = ["Venezuela",2,2]
+Vietnam = ["Vietnam",3,2]
+Yemen = ["Yemen",4,4]
+Zambia = ["Zambia",3,3]
+Zimbabwe = ["Zimbabwe",3,3]
+
+
+
+
+World = [Afghanistan,
+Albania,
+Algeria,
+Andorra,
+Angola,
+AntiguaandBarbuda,
+Argentina,
+Armenia,
+Australia,
+Austria,
+Azerbaijan,
+Bahamas,
+Bahrain,
+Bangladesh,
+Barbados,
+Belarus,
+Belgium,
+Belize,
+Benin,
+Bhutan,
+Bolivia,
+BosniaandHerzegovina,
+Botswana,
+Brazil,
+Brunei,
+Bulgaria,
+BurkinaFaso,
+Burundi,
+CapeVerde,
+Cambodia,
+Cameroon,
+Canada,
+CAR,
+Chad,
+Chile,
+China,
+Colombia,
+Comoros,
+DRC,
+ROC,
+CostaRica,
+CotedIvoire,
+Croatia,
+Cuba,
+Cyprus,
+Czechia,
+Denmark,
+Djibouti,
+Dominica,
+DominicanRepublic,
+EastTimor,
+Ecuador,
+Egypt,
+ElSalvador,
+EquatorialGuinea,
+Eritrea,
+Estonia,
+Eswatini,
+Ethiopia,
+Fiji,
+Finland,
+France,
+Gabon,
+Gambia,
+Georgia,
+Germany,
+Ghana,
+Greece,
+Grenada,
+Guatemala,
+Guinea,
+GuineaBissau,
+Guyana,
+Haiti,
+Honduras,
+HongKong,
+Hungary,
+Iceland,
+India,
+Indonesia,
+Iran,
+Iraq,
+Ireland,
+Israel,
+Italy,
+Jamaica,
+Japan,
+Jordan,
+Kazakhstan,
+Kenya,
+Kiribati,
+Kosovo,
+Kuwait,
+Kyrgyzstan,
+Laos,
+Latvia,
+Lebanon,
+Lesotho,
+Liberia,
+Libya,
+Liechtenstein,
+Lithuania,
+Luxembourg,
+Madagascar,
+Malawi,
+Malaysia,
+Maldives,
+Mali,
+Malta,
+MarshallIslands,
+Mauritania,
+Mauritius,
+Mexico,
+Micronesia,
+Moldova,
+Monaco,
+Mongolia,
+Montenegro,
+Morocco,
+Mozambique,
+Myanmar,
+Namibia,
+Nauru,
+Nepal,
+Netherlands,
+NewZealand,
+Nicaragua,
+Niger,
+Nigeria,
+NorthKorea,
+NorthMacedonia,
+Norway,
+Oman,
+Pakistan,
+Palau,
+Palestine,
+Panama,
+PapuaNewGuinea,
+Paraguay,
+Peru,
+Philippines,
+Poland,
+Portugal,
+Qatar,
+Romania,
+Russia,
+Rwanda,
+SaintKittsandNevis,
+SaintLucia,
+SaintVincentandtheGrenadines,
+Samoa,
+SanMarino,
+SaoTomeandPrincipe,
+SaudiArabia,
+Senegal,
+Serbia,
+Seychelles,
+SierraLeone,
+Singapore,
+Slovakia,
+Slovenia,
+SolomonIslands,
+Somalia,
+SouthAfrica,
+SouthKorea,
+SouthSudan,
+Spain,
+SriLanka,
+Sudan,
+Suriname,
+Sweden,
+Switzerland,
+Syria,
+Taiwan,
+Tajikistan,
+Tanzania,
+Thailand,
+Togo,
+Tonga,
+TrinidadandTobago,
+Tunisia,
+Turkey,
+Turkmenistan,
+Tuvalu,
+Uganda,
+Ukraine,
+UAE,
+UK,
+USA,
+Uruguay,
+Uzbekistan,
+Vanuatu,
+VaticanCity,
+Venezuela,
+Vietnam,
+Yemen,
+Zambia,
+Zimbabwe]
+
+Q1 = int(input('How important is it that the country you are visiting is highly developed? If it is **very important** enter 1, if it is **quite important** enter 2, if it is **not very important** enter 3, and if it is **not at all important** enter 4: '))
+
+i = 0
+n = len(World)
+while i < n:
+    if World[i][1] > Q1:
+        del World[i]
+        n = n -1
+    else:
+        i +=1
+
+Q2 = int(input('How important is it for you that LGBT people have equal rights? If it is **essential**, if it is **very important** enter 1, if it is **quite important** enter 2, if it is **not very important** enter 3, and if it is **not at all important** enter 4: '))
+
+i = 0
+n = len(World)
+while i < n:
+    if World[i][2] > Q2:
+        del World[i]
+        n = n -1
+    else:
+        i +=1
+
+x = 0
+y = len(World)
+while x < y:
+    print(World[x][0])
+    x +=1
